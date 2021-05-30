b1a57ce9f97fecef0dd092400360be7fa5d47920
Jonghun99 <83561366+Jonghun99@users.noreply.github.com>
2021-05-30 13:55:42 +0900

    Add files via upload

---
+++ Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDev_Do
+{
+    public class Common
+    {
+        public static string LogInId = string.Empty;
+        public static string LogInName = string.Empty;
+        public static string DbPath = "Data Source=DESKTOP-DDVAMMM; Initial Catalog=ITAcademyDB;User ID=jonghun;Password=1234";
+        public static string Permission = string.Empty;
+    }
+}
