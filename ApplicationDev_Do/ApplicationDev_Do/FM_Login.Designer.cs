524cc1f1f6e14417edb47ec5cbdb458618463a00
Jonghun99 <dlawhdgns99@naver.com>
2021-05-31 10:13:43 +0900

    little more

---
+++ ApplicationDev_Do/ApplicationDev_Do/FM_Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace ApplicationDev_Do
+{
+    partial class FM_Login
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUID = new System.Windows.Forms.TextBox();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUID
+            // 
+            this.txtUID.Location = new System.Drawing.Point(79, 576);
+            this.txtUID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUID.Name = "txtUID";
+            this.txtUID.Size = new System.Drawing.Size(237, 27);
+            this.txtUID.TabIndex = 0;
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(79, 611);
+            this.txtPW.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.PasswordChar = '*';
+            this.txtPW.Size = new System.Drawing.Size(237, 27);
+            this.txtPW.TabIndex = 1;
+            this.txtPW.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPW_KeyDown);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(141, 667);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(119, 32);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("바탕", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(79, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Learner\'s Web";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(372, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // FM_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ApplicationDev_Do.Properties.Resources.LoginDesign;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(404, 768);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.txtUID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FM_Login";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUID;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ApplicationDev_Do/ApplicationDev_Do/FM_Login.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace ApplicationDev_Do
+{
+    public partial class FM_Login : Form
+    {
+        private SqlConnection Connect = null;
+        public FM_Login()
+        {
+            InitializeComponent();
+            this.Tag = "FAIL";
+        }
+
+        private Point mousePoint;
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                Location = new Point(this.Left - (mousePoint.X - e.X),
+                    this.Top - (mousePoint.Y - e.Y));
+            }
+        }
+
+        public int failcount = 0;
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string strCon = "Data Source = 222.235.141.8; " +
+                "Initial Catalog = AppDev;" +
+                "User ID=kfqs1;Password=1234";
+            Connect = new SqlConnection(strCon);
+
+            Connect.Open();//데이터베이스에 접속한다.
+            if (Connect.State != System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("데이터베이스 연결 실패. ");
+                return;
+
+            }
+
+
+            String sLogInId = string.Empty;
+            string sPerPw = string.Empty;
+
+
+            sLogInId = txtUID.Text;
+            sPerPw = txtPW.Text;
+
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT PERMISSION,NAME,PASSWORD,USERID FROM TB_5_USER WHERE USERID = '" + sLogInId + "'", Connect);
+            //데이터를 담을 그릇
+            DataTable DtTemp = new DataTable();
+            //어댑터 실행 후 그릇의 데이터 담기
+            adapter.Fill(DtTemp);
+
+            // 데이터가 없는 경우 사용자가 없다고 메세지 및 리턴
+
+
+            if (DtTemp.Rows.Count == 0)
+            {
+                MessageBox.Show("사용자 정보가 없습니다.");
+                failcount = failcount + 1;
+                if (failcount == 3)
+                {
+                    MessageBox.Show("3회 실패!");
+                    Application.Exit();
+                    return;
+                }
+
+
+
+            }
+
+            else if (DtTemp.Rows[0]["PASSWORD"].ToString() != sPerPw)//row는 이름을 때릴 수 있다네?
+            {
+
+                MessageBox.Show("ID 또는 비밀번호가 일치하지 않습니다.");
+
+                failcount = failcount + 1;
+                if (failcount == 3)
+                {
+                    MessageBox.Show("3회 실패!");
+                    Application.Exit();
+                    return;
+                }
+            }
+
+            else if ((DtTemp.Rows[0]["PASSWORD"].ToString() == sPerPw))
+            {
+                MessageBox.Show("환영합니다!");
+                Common.LogInId = txtUID.Text;
+                Common.LogInName = DtTemp.Rows[0]["NAME"].ToString();
+                Common.Permission = DtTemp.Rows[0]["PERMISSION"].ToString();
+                //this.Tag = DtTemp.Rows[0]["PERMISSION"].ToString(); //요건 종료될때까지 살아있나? 아니다.
+                this.Tag = DtTemp.Rows[0]["NAME"].ToString();
+                this.Close();
+
+                Connect.Close();
+
+            }
+
+
+
+
+
+
+        }
+
+        private void txtPW_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_login_Click(null, null);
+            }
+        }
+    }
+}
