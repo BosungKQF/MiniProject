9f84d62630effef200e99e884a56c756d4ab9a84
pass0585@gmail.com <pass0585@gmail.com>
2021-06-01 16:32:42 +0900

    알림장 중간

--- ApplicationDev_Do/ApplicationDev_Do/FM_NoticeSend.Designer.cs
+++ ApplicationDev_Do/ApplicationDev_Do/FM_NoticeSend.Designer.cs
@@ -30,21 +30,23 @@ namespace ApplicationDev_Do
         private void InitializeComponent()
         {
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.lbStudent = new System.Windows.Forms.ListBox();
             this.btnSend = new System.Windows.Forms.Button();
-            this.cboNoticeStudent = new System.Windows.Forms.ComboBox();
             this.cboNoticeClass = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.rtxtNotice = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.lbStudent);
             this.groupBox2.Controls.Add(this.btnSend);
-            this.groupBox2.Controls.Add(this.cboNoticeStudent);
             this.groupBox2.Controls.Add(this.cboNoticeClass);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
@@ -58,39 +60,28 @@ namespace ApplicationDev_Do
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
-            // checkedListBox1
+            // lbStudent
             // 
-            this.checkedListBox1.CheckOnClick = true;
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(282, 54);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(136, 136);
-            this.checkedListBox1.TabIndex = 11;
-            this.checkedListBox1.Visible = false;
+            this.lbStudent.FormattingEnabled = true;
+            this.lbStudent.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.lbStudent.ItemHeight = 20;
+            this.lbStudent.Location = new System.Drawing.Point(307, 28);
+            this.lbStudent.Name = "lbStudent";
+            this.lbStudent.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbStudent.Size = new System.Drawing.Size(136, 24);
+            this.lbStudent.TabIndex = 11;
+            this.lbStudent.Click += new System.EventHandler(this.lbStudent_Click);
+            this.lbStudent.SelectedValueChanged += new System.EventHandler(this.lbStudent_SelectedValueChanged);
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(627, 13);
+            this.btnSend.Location = new System.Drawing.Point(721, 28);
             this.btnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(129, 59);
             this.btnSend.TabIndex = 10;
             this.btnSend.Text = "보내기";
             this.btnSend.UseVisualStyleBackColor = true;
-            // 
-            // cboNoticeStudent
-            // 
-            this.cboNoticeStudent.DropDownHeight = 240;
-            this.cboNoticeStudent.DropDownWidth = 240;
-            this.cboNoticeStudent.FormattingEnabled = true;
-            this.cboNoticeStudent.IntegralHeight = false;
-            this.cboNoticeStudent.Items.AddRange(new object[] {
-            "전체"});
-            this.cboNoticeStudent.Location = new System.Drawing.Point(282, 28);
-            this.cboNoticeStudent.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.cboNoticeStudent.Name = "cboNoticeStudent";
-            this.cboNoticeStudent.Size = new System.Drawing.Size(136, 28);
-            this.cboNoticeStudent.TabIndex = 9;
             // 
             // cboNoticeClass
             // 
@@ -99,7 +90,7 @@ namespace ApplicationDev_Do
             "전체",
             "스마트팩토리",
             "빅데이터"});
-            this.cboNoticeClass.Location = new System.Drawing.Point(68, 28);
+            this.cboNoticeClass.Location = new System.Drawing.Point(96, 28);
             this.cboNoticeClass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cboNoticeClass.Name = "cboNoticeClass";
             this.cboNoticeClass.Size = new System.Drawing.Size(136, 28);
@@ -108,7 +99,7 @@ namespace ApplicationDev_Do
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 32);
+            this.label2.Location = new System.Drawing.Point(65, 32);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(24, 20);
             this.label2.TabIndex = 7;
@@ -117,7 +108,7 @@ namespace ApplicationDev_Do
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 32);
+            this.label1.Location = new System.Drawing.Point(262, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 20);
             this.label1.TabIndex = 6;
@@ -132,6 +123,24 @@ namespace ApplicationDev_Do
             this.rtxtNotice.Size = new System.Drawing.Size(894, 493);
             this.rtxtNotice.TabIndex = 0;
             this.rtxtNotice.Text = "";
+            this.rtxtNotice.Click += new System.EventHandler(this.rtxtNotice_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(587, 27);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "받는 사람";
             // 
             // FM_NoticeSend
             // 
@@ -153,10 +162,11 @@ namespace ApplicationDev_Do
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RichTextBox rtxtNotice;
         private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.ComboBox cboNoticeStudent;
         private System.Windows.Forms.ComboBox cboNoticeClass;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ListBox lbStudent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
